285a32c433d997b7e51bc4cb5959c6bd48a76014
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-04-19 23:39:47 -0400

    Aula 2 - Atividade 4 - Imprimindo a soma dos numeros de 1 ate 1000

---
+++ src/ImprimeSoma.java
@@ -0,0 +1,10 @@
+
+public class ImprimeSoma {
+	public static void main(String[] args) {
+		int soma = 0;
+		for (int i = 1; i <= 1000; i++) {
+			soma = i + soma;
+			System.out.println(soma);
+		}
+	}
+}
