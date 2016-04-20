31f559159c5a167e78faac2c6aa6e7e02b0093cf
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-04-19 22:47:27 -0400

    Aula 1 - Atividade 4 - Imprimindo duas linhas com o \n

--- src/MeuProgramaModificado.java
+++ src/MeuProgramaModificado.java
@@ -1,7 +1,6 @@
 
 public class MeuProgramaModificado {
 	public static void main(String[] args) {
-		System.out.println("Olá Java");
-		System.out.println("Olá Java");
+		System.out.println("Olá Java \nOlá de novo");
 	}
 }
