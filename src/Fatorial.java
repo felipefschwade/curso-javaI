dd564f5937dfcb74e3f6e64c710de9791453ff49
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-04-21 13:51:10 -0400

    Aula 2 - Atividade 7 - Usando o long para imprimir mais fatoriais

--- src/Fatorial.java
+++ src/Fatorial.java
@@ -1,8 +1,8 @@
 
 public class Fatorial {
 	public static void main(String[] args) {
-		int fatorial = 1;
-        for (int n = 1; n <= 10; n++) {
+		long fatorial = 1;
+        for (int n = 1; n <= 20; n++) {
             fatorial = fatorial * n;
             System.out.println("fatorial de (" + n + ") = " + fatorial);
         }
