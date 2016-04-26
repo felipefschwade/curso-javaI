1f5e2927f4ae394ba471690f4462e7b39f8f5e01
Felipe Faé Schwade <felipefaeschwade@gmail.com>
2016-04-25 20:46:11 -0400

    Aula 4 - Exercicio 2 - Criando o método adiciona

--- src/Empresa.java
+++ src/Empresa.java
@@ -1,4 +1,13 @@
 
 public class Empresa {
-
+	String nome;
+	int cnpj;
+	Funcionario[] funcionario;
+	
+	void adiciona(Funcionario funcionario, int posicao) {
+		if (this.funcionario == null) {
+			this.funcionario[posicao] = funcionario;
+		}
+		
+	}
 }
